c84fdb894c2fc117ff948c654bd72d96b9ae866e
Jp Keys <jpmusik29@gmail.com>
2025-03-07 12:20:32 -0500

    Search bar location restriction

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -9,6 +9,49 @@ import Button from "@mui/material/Button"
 
 const libraries = ["places"]
 
+// function SearchBar() {
+//   const inputref = useRef(null)
+//   const { isLoaded } = useJsApiLoader({
+//     id: "google-map-script",
+//     googleMapsApiKey: import.meta.env.VITE_GOOGLEMAPS_API_KEY,
+//     libraries,
+//   })
+
+//   const requestLocation = async () => {
+//     try {
+//       const response = await fetch("https://places.googleapis.com/v1/places:searchNearby", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+//         body: JSON.stringify({
+//           "includedTypes": [
+//             "apartment_building", "apartment_complex", "condominium_complex", "street_address"
+//           ],
+//           "locationRestriction": {
+//             "circle": {
+//               "center": {
+//                 "latitude": 40.6958,
+//                 "longitude": -73.9171
+//               },
+//               "radius": 500.0
+//             }
+//           }
+//         })
+//       });
+
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+
+//       const data = await response.json();
+//       console.log(data); 
+//     } catch (error) {
+//       console.error("Error fetching location:", error);
+//     }
+// };
+
+
 function SearchBar() {
   const inputref = useRef(null)
   const { isLoaded } = useJsApiLoader({
@@ -18,30 +61,52 @@ function SearchBar() {
   })
   const [area, setArea] = useState({ short_name: "", long_name: "" })
   const navigate = useNavigate()
-  console.log(isLoaded)
+  const [ isLoading, setIsLoading ] = useState(false);
 
   const handleOnPlacesChanged = () => {
     let address = inputref.current.getPlaces()
     console.log(address)
-    let locality = null
+
+    const nycBounds = {
+      low: {
+        latitude: 40.4774,
+        longitude: -74.2591
+      },
+      high: {
+        latitude: 40.9176,
+        longitude: -73.7004,
+      },
+    };
+
     if (address.length !== 0) {
-      let formatted_address = address[0].formatted_address
-      // let find_locality = address_components.filter((item) => item.types.includes("locality"))
-      // let short_name = find_locality[0]?.short_name
-      // let long_name = find_locality[0]?.long_name
-      setArea({ short_name: formatted_address, long_name: formatted_address })
+      let formatted_address = address[0].formatted_address;
+      let placeLat = address[0].geometry.location.lat();
+      let placeLng = address[0].geometry.location.lng();
+
+      if (
+        placeLat >= nycBounds.low.latitude && placeLat <= nycBounds.high.latitude &&
+        placeLng >= nycBounds.low.longitude && placeLng <= nycBounds.high.longitude
+      ) {
+        setArea({ short_name: formatted_address, long_name: formatted_address })
+      } else {
+        console.log("Your search sucks!");    
+      }
     }
-  }
+  };
 
   const handleSearch = () => {
-    console.log("Area object:", area)
-    if (area && area.short_name && area.long_name) {
-      const searchUrl = `searchresults?area_short_name=${area.short_name}&area_long_name=${area.long_name}`
-      navigate(searchUrl)
-    } else {
-      setArea({ short_name: "", long_name: "" })
+    try {
+      if (area && area.short_name && area.long_name) {
+        const searchUrl = `searchresults?area_short_name=${area.short_name}&area_long_name=${area.long_name}`
+        navigate(searchUrl)
+      } else {
+        setArea({ short_name: "", long_name: "" })
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
 
   return (
     <div style={{ marginTop: "10%", textAlign: "center" }}>
@@ -69,13 +134,15 @@ function SearchBar() {
           />
         </StandaloneSearchBox>
       )}
+
       <Button
         variant="contained"
         size="large"
         sx={{ marginTop: "15px", backgroundColor:"#243156", textTransform:"none" }}
         onClick={handleSearch}
+        disabled={isLoading}
       >
-        Search
+       Search
       </Button>
     </div>
   )
