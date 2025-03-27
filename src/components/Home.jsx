63c2644c393819b061824cd004770673bc3866f0
Jp Keys <jpmusik29@gmail.com>
2025-03-27 16:19:59 -0400

    Carousel implementation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { SearchBar } from "./SearchBar";
 import Post from "./Post";
 import client from '../sanityClient'
-import { Box, Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Box, Button, Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
 import { BoxStyled, BoxPostStyled } from "./HomeStyles";
 import { HomePosts } from "./HomePosts";
 import { useQuery } from "@tanstack/react-query";
@@ -26,10 +26,14 @@ const useallRoomPostings = () => {
 }
 
 export const Home = ({message}) => {
-  
+  const [ currentIndex, setCurrentIndex ] = useState(0)
   const { data : roomPosting, error, isLoading} = useallRoomPostings();
-  // console.log(JSON.stringify(roomPosting));
 
+  
+  const handleNextPic = ({postingDetails}) => {
+    setCurrentIndex(prev => (prev < postingDetails.images.length - 1 ? prev + 1 : 0));
+  }
+  
   const builder = imageUrlBuilder(client)
   const urlFor = (source) => {
     if(!source) {
@@ -52,28 +56,35 @@ export const Home = ({message}) => {
           {isLoading ?  
           <CircularProgress /> 
           : [...roomPosting]
-          .sort(() => Math.random() - 0.5)
+          // .sort(() => Math.random() - 0.5)
           .slice(0, 9).map((post, i) => (
             <Card key={i}  >
-                <CardHeader>
+              <CardHeader>
+              </CardHeader>
 
-                </CardHeader>
-                <CardMedia
-                component="img"
-                height="180"
-                image={urlFor(post.images?.[0].asset._ref)}
-                alt="room"
-                />
-                <CardContent >
-                  <Typography variant="inherit" sx={{ fontWeight: "bold", fontSize: "20px" }}>${post.price} /mo</Typography>
-                  <Box
-                   sx={{ display: "flex", gap: 1, backgroundColor: "#E0E0E0", borderRadius: "15px", padding: "4px 8px", width:"210px", color: "grey.700"}}
-                   >
-                    <Typography>{post.neighborhood},</Typography>
-                    <Typography>{post.area}</Typography>
-                  </Box>
-                </CardContent>
-              </Card>
+              <CardMedia
+              component="img"
+              height="180"
+              // image={urlFor(image.asset._ref)}
+              image={urlFor(postingDetails?.images[currentIndex].asset._ref)}
+              alt="room"
+              />
+
+              <Button
+              onClick={handleNextPic}
+              >Next ► </Button>
+
+
+              <CardContent >
+                <Typography variant="inherit" sx={{ fontWeight: "bold", fontSize: "20px" }}>${post.price} /mo</Typography>
+                <Box
+                  sx={{ display: "flex", gap: 1, backgroundColor: "#E0E0E0", borderRadius: "15px", padding: "4px 8px", width:"210px", color: "grey.700"}}
+                  >
+                  <Typography>{post.neighborhood},</Typography>
+                  <Typography>{post.area}</Typography>
+                </Box>
+              </CardContent>
+            </Card>
             ))}
         </BoxPostStyled>
       </BoxStyled>
