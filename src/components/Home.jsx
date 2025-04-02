aac49e68761b930e15ead4a0c7b17ba175043058
Jp Keys <jpmusik29@gmail.com>
2025-04-02 17:29:09 -0400

    Responsive grid using theme breakpoints

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ import { SearchBar } from "./SearchBar";
 import Post from "./Post";
 import client from '../sanityClient'
 import { Box, Button, Card, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
-import { BoxStyled, BoxPostStyled } from "./HomeStyles";
+import { BoxStyled, BoxPostStyled, TypograStyled } from "./HomeStyles";
 import { HomePosts } from "./HomePosts";
 import { useQuery } from "@tanstack/react-query";
 import imageUrlBuilder from '@sanity/image-url'
 import { CircularProgress } from "@mui/material";
-
+import { CardPosting } from "./postingCards/Card";
 
 const useallRoomPostings = () => {
   const fetchAllPostings = async () => {
@@ -26,29 +26,11 @@ const useallRoomPostings = () => {
 }
 
 export const Home = ({message}) => {
-  const [ currentIndex, setCurrentIndex ] = useState(0)
   const { data : roomPosting, error, isLoading} = useallRoomPostings();
-
-  
-  const handleNextPic = ({post}) => {
-    setCurrentIndex(prev => (prev < post?.images.length - 1 ? prev + 1 : 0));
-  }
-  
-  const builder = imageUrlBuilder(client)
-  const urlFor = (source) => {
-    if(!source) {
-      console.log('No image source available');
-      return "";
-    }
-    return builder.image(source).url()
-  }
       
       return (
-        <div style={{ height: "100vh", backgroundColor:"#F0F0F0" }}>
+        <div style={{ height: "100vh", backgroundColor:"#F5F5F5" }}>
       <BoxStyled>
-        <Typography>
-          {message}
-        </Typography>
         <div>
           <SearchBar />
         </div>
@@ -59,30 +41,13 @@ export const Home = ({message}) => {
           // .sort(() => Math.random() - 0.5)
           .slice(0, 9).map((post, i) => (
             <Card key={i}  >
-              <CardHeader>
-              </CardHeader>
-
-              <CardMedia
-              component="img"
-              height="180"
-              // image={urlFor(image.asset._ref)}
-              image={urlFor(post?.images[currentIndex].asset._ref)}
-              alt="room"
-              />
-
-              <Button
-              onClick={handleNextPic}
-              >Next ► </Button>
-
-
+              <CardPosting roomPosting={roomPosting} />
               <CardContent >
                 <Typography variant="inherit" sx={{ fontWeight: "bold", fontSize: "20px" }}>${post.price} /mo</Typography>
-                <Box
-                  sx={{ display: "flex", gap: 1, backgroundColor: "#E0E0E0", borderRadius: "15px", padding: "4px 8px", width:"210px", color: "grey.700"}}
-                  >
+                <TypograStyled>
                   <Typography>{post.neighborhood},</Typography>
                   <Typography>{post.area}</Typography>
-                </Box>
+                </TypograStyled>
               </CardContent>
             </Card>
             ))}
--- src/components/HomeStyles.jsx
+++ src/components/HomeStyles.jsx
@@ -1,4 +1,4 @@
-import { styled, Box, Typography, Card } from '@mui/material';
+import { styled, Box, Typography, Card, Grid2 } from '@mui/material';
 
 export const BoxStyled = styled(Box)(({theme}) => ({
     backgroundImage:
@@ -8,12 +8,33 @@ export const BoxStyled = styled(Box)(({theme}) => ({
     backgroundRepeat: "no-repeat",
     height: "50%",
     padding: 6,
+
 }))
 
 export const BoxPostStyled = styled(Box)(({theme}) => ({
+    flexGrow: 1,
     display: 'grid',
-     gridTemplateColumns: 'repeat(3, 1fr)',
-      gap:15, marginTop: "20vh",
-      padding: "20px",
-      backgroundColor:"#F5F5F5"
+    // gridTemplateColumns: 'repeat(3, 1fr)',
+    gap:15, marginTop: "20vh",
+    padding: "20px",
+    backgroundColor: "#F5F5F5",
+
+    [theme.breakpoints.up('sm')]: {
+        gridTemplateColumns: 'repeat(2, 1fr)',
+    },
+    [theme.breakpoints.up('md')]: {
+        gridTemplateColumns: 'repeat(3, 1fr)',
+    },
+    // [theme.breakpoints.up('lg')]: {
+    //     gridTemplateColumns: 'repeat(4, 1fr)',
+    // },
+}))
+
+export const TypograStyled = styled(Box)(({theme}) => ({
+    display: "flex", 
+    gap: 1, backgroundColor: "#E0E0E0", 
+    borderRadius: "15px", 
+    padding: "4px 8px", width:"210px", 
+    color: "grey.700"
+
 }))
