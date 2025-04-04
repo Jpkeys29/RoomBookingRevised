9a84442f4e759235d5fe1fee92c0bf6368f83b18
Jp Keys <jpmusik29@gmail.com>
2025-04-04 08:43:11 -0400

    CardSearch implementation

---
+++ src/components/postingCards/CardSearch.jsx
@@ -0,0 +1,45 @@
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent'
+import { CardMedia } from '@mui/material'
+import Typography from '@mui/material/Typography'
+import { Container, Image, Badge } from './Cardstyles'
+import { Box } from '@mui/material'
+import imageUrlBuilder from '@sanity/image-url'
+import client from '../../sanityClient'
+
+
+export const CardSearch = ({posting})  => {
+    const builder = imageUrlBuilder(client)
+    const urlFor = (source) => {
+        return builder.image(source).url()
+    }
+
+  return (
+    <Container>
+      <CardContent  >
+        {posting && posting.images && posting.images.length !== 0 &&
+        <CardMedia
+        component="img"
+        image={ urlFor(posting?.images[0])}
+        loading='lazy'
+        style={{ aspectRatio: '3/2'}}
+        /> 
+        }
+        <CardContent>
+          <Typography variant="subtitle2">
+            {/* <Badge>NEW</Badge> 3 BEDS &bull; 2 BATHS */}
+          </Typography>
+          <Typography variant="h6" component="h2" noWrap>
+            {posting?.description}
+          </Typography>
+          <Typography variant="body1" color="textSecondary" component="p">
+            {posting?.neighborhood}({posting?.area})
+          </Typography>
+          <Typography variant="body1" color="textSecondary" component="p">
+            ${posting?.price}
+          </Typography>
+        </CardContent>
+      </CardContent>
+    </Container>
+  )
+}
