202863e5efd10d9e89f60636c38b1705318ee679
Jp Keys <jpmusik29@gmail.com>
2025-05-06 07:09:50 -0400

    Card text fixed

--- src/components/postingCards/Card.jsx
+++ src/components/postingCards/Card.jsx
@@ -9,6 +9,7 @@ import client from '../../sanityClient'
 import { useState } from 'react';
 import SearchResults from '../SearchResults';
 import Avatar from '@mui/material/Avatar';
+import { TypograStyled } from '../HomeStyles';
 
 export const CardPosting = ({roomPosting}) => {
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -24,15 +25,8 @@ export const CardPosting = ({roomPosting}) => {
   }; 
 
   return (
-    <Container style={{position: 'relative'}}>
-    <Card >
-        {/* <CardHeader>
-          <Avatar
-          sx={{ width: 24, height: 24, bgcolor: 'red'  }}
-          >
-            R
-          </Avatar>  
-        </CardHeader> */}
+    <Container >
+      <Card style={{position: 'relative'}}>
         {roomPosting.images?.length > 0 && (
           <CardMedia
           component="img"
@@ -53,15 +47,20 @@ export const CardPosting = ({roomPosting}) => {
           <Typography variant="h6" component="h2" noWrap>
             {roomPosting?.description}
           </Typography>
-          <Typography variant="body1" color="textSecondary" component="p">
+          {/* <Typography variant="body1" color="textSecondary" component="p">
             {roomPosting?.neighborhood}({roomPosting?.area})
-          </Typography>
-          <Typography variant="body1" color="textSecondary" component="p">
+          </Typography> */}
+          <Typography variant="body1" color="textSecondary" component="p" fontSize= '18px'>
             ${roomPosting?.price}
           </Typography>
+          <TypograStyled>
+        <Typography>{roomPosting?.neighborhood}, </Typography>
+        <Typography>{roomPosting?.area}</Typography>
+        </TypograStyled>
+
         </CardContent>
       </Card>
-    </Container>
+        </Container>
   )
 }
 
