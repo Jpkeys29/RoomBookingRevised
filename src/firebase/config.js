843e64207569ab0552c80385a461072c8bdb4a4c
Jp Keys <jpmusik29@gmail.com>
2025-06-02 08:20:21 -0400

    firebase config component

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,9 @@ import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 
 // import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  // apiKey: "AIzaSyAHR_2y554dKc3HGjD5nVYVwBdXXble4vg",
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_AUTH_PROJECT_ID,
   storageBucket: import.meta.env.VITE_AUTH_storageBucket,
