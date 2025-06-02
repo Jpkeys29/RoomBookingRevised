4f162b74ad2d2afee60fce1dc5b98c9388852a38
Jp Keys <jpmusik29@gmail.com>
2025-06-02 07:23:44 -0400

    SanityClient

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -3,11 +3,12 @@ import {createClient} from '@sanity/client';
     const client = createClient({
     projectId: 'yydt0bri',
     dataset: 'production',
-    useCdn: false, // set to `false` to bypass the edge cache
-    apiVersion: '2024-05-03', // use current date (YYYY-MM-DD) to target the latest API version
-    token: import.meta.env.VITE_sanityToken // Only if you want to update content with the client
+    useCdn: false,
+    apiVersion: '2024-05-03', 
+    token: import.meta.env.VITE_SANITY_TOKEN, 
   })
-  console.log(import.meta.env.VITE_sanityToken)
+
   export default client;
+
 
 
